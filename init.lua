7a2b1b8720bd5b52499515403ee914845ae28ece
Zakhary Kaplan <me@zakhary.dev>
2023-09-11 00:00:00 -0400

    chore: add ascii banner

--- init.lua
+++ init.lua
@@ -4,5 +4,13 @@
 -- SPDX-License-Identifier: MIT
 -- Vim:         set fdl=0 fdm=marker:
 
+-- ooooo      ooo              o8o
+-- `888b.     `8'              `"'
+--  8 `88b.    8  oooo    ooo oooo  ooo. .oo.  .oo.
+--  8   `88b.  8   `88.  .8'  `888  `888P"Y88bP"Y88b
+--  8     `88b.8    `88..8'    888   888   888   888
+--  8       `888     `888'     888   888   888   888
+-- o8o        `8      `8'     o888o o888o o888o o888o
+
 -- load vimrc
 require("vimrc").setup()
