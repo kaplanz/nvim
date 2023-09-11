fe513646a9a67c214ac32ea91b4e078a10517a81
Zakhary Kaplan <me@zakhary.dev>
2023-09-11 00:00:00 -0400

    feat(mini): update clue setup

--- lua/vimrc/mod/ui/menu.lua
+++ lua/vimrc/mod/ui/menu.lua
@@ -22,45 +22,54 @@ vim.cmd([[
 
 -- Create next key clues
 mini.clue.setup {
-  triggers = {
-    -- Leader triggers
-    { mode = 'n', keys = '<Leader>' },
-    { mode = 'x', keys = '<Leader>' },
-
-    -- Built-in completion
-    { mode = 'i', keys = '<C-x>' },
-
-    -- `g` key
-    { mode = 'n', keys = 'g' },
-    { mode = 'x', keys = 'g' },
-
-    -- Marks
-    { mode = 'n', keys = "'" },
-    { mode = 'n', keys = '`' },
-    { mode = 'x', keys = "'" },
-    { mode = 'x', keys = '`' },
-
-    -- Registers
-    { mode = 'n', keys = '"' },
-    { mode = 'x', keys = '"' },
-    { mode = 'i', keys = '<C-r>' },
-    { mode = 'c', keys = '<C-r>' },
-
-    -- Window commands
-    { mode = 'n', keys = '<C-w>' },
-
-    -- `z` key
-    { mode = 'n', keys = 'z' },
-    { mode = 'x', keys = 'z' },
-  },
-
+  -- Array of extra clues to show
   clues = {
     -- Enhance this by adding descriptions for <Leader> mapping groups
     mini.clue.gen_clues.builtin_completion(),
     mini.clue.gen_clues.g(),
     mini.clue.gen_clues.marks(),
-    mini.clue.gen_clues.registers(),
-    mini.clue.gen_clues.windows(),
+    mini.clue.gen_clues.registers {
+      show_contents = true,
+    },
+    mini.clue.gen_clues.windows {
+      submode_move = false,
+      submode_navigate = false,
+      submode_resize = true,
+    },
     mini.clue.gen_clues.z(),
+  },
+
+  -- Array of opt-in triggers which start custom key query process.
+  -- **Needs to have something in order to show clues**.
+  triggers = {
+    -- Leader triggers
+    { mode = "n", keys = "<Leader>" },
+    { mode = "x", keys = "<Leader>" },
+    -- Built-in completion
+    { mode = "i", keys = "<C-x>" },
+    -- `g` key
+    { mode = "n", keys = "g" },
+    { mode = "x", keys = "g" },
+    -- Marks
+    { mode = "n", keys = "'" },
+    { mode = "n", keys = "`" },
+    { mode = "x", keys = "'" },
+    { mode = "x", keys = "`" },
+    -- Registers
+    { mode = "n", keys = '"' },
+    { mode = "x", keys = '"' },
+    { mode = "i", keys = "<C-r>" },
+    { mode = "c", keys = "<C-r>" },
+    -- Window commands
+    { mode = "n", keys = "<C-w>" },
+    -- `z` key
+    { mode = "n", keys = "z" },
+    { mode = "x", keys = "z" },
+  },
+
+  -- Clue window settings
+  window = {
+    -- Delay before showing clue window
+    delay = 0,
   },
 }
