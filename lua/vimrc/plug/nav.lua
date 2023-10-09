f0684beba08f65b3db4364374de4f27739a45c6a
Zakhary Kaplan <me@zakhary.dev>
2023-10-09 00:00:00 -0400

    feat(flash): install

--- lua/vimrc/plug/nav.lua
+++ lua/vimrc/plug/nav.lua
@@ -4,6 +4,8 @@
 -- SPDX-License-Identifier: MIT
 
 return {
+  -- Navigate code with search labels
+  { "folke/flash.nvim", opts = {} },
   -- File explorer written in lua
   { "kyazdani42/nvim-tree.lua", dependencies = {
     "kyazdani42/nvim-web-devicons",
