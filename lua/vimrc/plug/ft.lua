343a5d4a0fd552a0487ee5a30fce58ddd8c81d94
Zakhary Kaplan <me@zakhary.dev>
2024-01-19 00:00:00 -0500

    feat(typst): install

---
+++ lua/vimrc/plug/ft.lua
@@ -0,0 +1,9 @@
+-- File:        ft.lua
+-- Author:      Zakhary Kaplan <https://zakhary.dev>
+-- Created:     16 Jan 2024
+-- SPDX-License-Identifier: NONE
+
+return {
+  -- Typst filetype
+  { "kaarmu/typst.vim", ft = "typst" },
+}
