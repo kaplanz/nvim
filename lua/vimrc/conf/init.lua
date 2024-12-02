ce215e2600170bc6df55498db70f94175ed36db6
Zakhary Kaplan <me@zakhary.dev>
2024-12-02 00:00:00 -0500

    revert(dap): uninstall
    This reverts commit a1fdb790f01ed1a72b9d42e5927fc3b62402d7d4.

--- lua/vimrc/conf/init.lua
+++ lua/vimrc/conf/init.lua
@@ -3,8 +3,5 @@
 -- Created:     20 Dec 2022
 -- SPDX-License-Identifier: MIT
 
--- Debug Adapter Protocol
-require("vimrc.conf.dap")
-
 -- Language Server Protocol
 require("vimrc.conf.lsp")
--- lua/vimrc/plug/dap.lua
+++
@@ -1,9 +0,0 @@
--- File:        dap.lua
--- Author:      Zakhary Kaplan <https://zakhary.dev>
--- Created:     11 Sep 2023
--- SPDX-License-Identifier: MIT
-
--- Debug Adapter Protocol client
-return {
-  "mfussenegger/nvim-dap",
-}
