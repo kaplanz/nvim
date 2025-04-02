6c69f077cc7072abe8c444933471f8741993fb56
Zakhary Kaplan <me@zakhary.dev>
2025-04-02 00:00:00 -0400

    revert(flash): uninstall
    This reverts commit f0684beba08f65b3db4364374de4f27739a45c6a.

--- lua/vimrc/plug/flash.lua
+++
@@ -1,9 +0,0 @@
--- File:        flash.lua
--- Author:      Zakhary Kaplan <https://zakhary.dev>
--- Created:     11 Sep 2023
--- SPDX-License-Identifier: MIT
-
-return {
-  -- Navigate code with search labels
-  { "folke/flash.nvim", opts = {} },
-}
