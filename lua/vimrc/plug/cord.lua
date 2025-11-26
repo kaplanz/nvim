18629055002baca12fee05b8f67591799ac6e751
Zakhary Kaplan <me@zakhary.dev>
2025-11-26 17:30:00 -0500

    feat(cord): install

---
+++ lua/vimrc/plug/cord.lua
@@ -0,0 +1,11 @@
+-- File:        cord.lua
+-- Author:      Zakhary Kaplan <https://zakhary.dev>
+-- Created:     27 Nov 2024
+-- SPDX-License-Identifier: MIT
+
+return {
+  'vyfor/cord.nvim',
+  build = ':Cord update',
+  event = 'VeryLazy',
+  opts = {},
+}
