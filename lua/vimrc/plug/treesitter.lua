677f4ba785edc2a5d8ad24c90f82f59896660210
Zakhary Kaplan <me@zakhary.dev>
2026-04-30 12:57:23 -0400

    fix(treesitter): migrate to v0.12

--- lua/vimrc/plug/treesitter.lua
+++ lua/vimrc/plug/treesitter.lua
@@ -6,12 +6,72 @@
 return {
   -- Treesitter configurations
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
+  branch = "main",
   dependencies = {
     -- Show code context
     "nvim-treesitter/nvim-treesitter-context",
-    -- Syntax aware text-objects
-    "nvim-treesitter/nvim-treesitter-textobjects",
+    -- Syntax aware text-objects, select, move, swap, and peek support
+    {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      branch = "main",
+      config = function()
+        require("nvim-treesitter-textobjects").setup({
+          select = {
+            lookahead = true,
+          },
+          move = {
+            set_jumps = true, -- whether to set jumps in the jumplist
+          },
+        })
+
+        local select = require("nvim-treesitter-textobjects.select")
+        local swap   = require("nvim-treesitter-textobjects.swap")
+        local move   = require("nvim-treesitter-textobjects.move")
+
+        -- Text object selection
+        -- You can use the capture groups defined in textobjects.scm
+        vim.keymap.set({ "x", "o" }, "aa", function()
+          select.select_textobject("@parameter.outer", "textobjects")
+        end)
+        vim.keymap.set({ "x", "o" }, "ia", function()
+          select.select_textobject("@parameter.inner", "textobjects")
+        end)
+        vim.keymap.set({ "x", "o" }, "ac", function()
+          select.select_textobject("@class.outer", "textobjects")
+        end)
+        vim.keymap.set({ "x", "o" }, "ic", function()
+          select.select_textobject("@class.inner", "textobjects")
+        end)
+        vim.keymap.set({ "x", "o" }, "af", function()
+          select.select_textobject("@function.outer", "textobjects")
+        end)
+        vim.keymap.set({ "x", "o" }, "if", function()
+          select.select_textobject("@function.inner", "textobjects")
+        end)
+
+        -- Swap text objects
+        vim.keymap.set("n", "<M-l>", function()
+          swap.swap_next("@parameter.inner")
+        end)
+        vim.keymap.set("n", "<M-h>", function()
+          swap.swap_previous("@parameter.inner")
+        end)
+
+        -- Go to next/previous text object
+        vim.keymap.set({ "n", "x", "o" }, "]m", function()
+          move.goto_next_start({ "@function.outer", "@class.outer" }, "textobjects")
+        end)
+        vim.keymap.set({ "n", "x", "o" }, "]M", function()
+          move.goto_next_end({ "@function.outer", "@class.outer" }, "textobjects")
+        end)
+        vim.keymap.set({ "n", "x", "o" }, "[m", function()
+          move.goto_previous_start({ "@function.outer", "@class.outer" }, "textobjects")
+        end)
+        vim.keymap.set({ "n", "x", "o" }, "[M", function()
+          move.goto_previous_end({ "@function.outer", "@class.outer" }, "textobjects")
+        end)
+      end,
+    },
   },
   event = { "BufReadPost", "BufNewFile" },
   build = ":TSUpdate",
@@ -55,56 +115,6 @@ return {
     -- Indentation based on treesitter for the `=` operator
     indent = {
       enable = true,
-    },
-
-    -- Syntax aware text-objects, select, move, swap, and peek support
-    textobjects = {
-      -- Text object selection
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["aa"] = "@parameter.outer",
-          ["ia"] = "@parameter.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-        },
-      },
-      -- Swap text objects
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<M-l>"] = { "@parameter.inner" },
-        },
-        swap_previous = {
-          ["<M-h>"] = { "@parameter.inner" },
-        },
-      },
-      -- Go to next/previous text object
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          ["]m"] = { "@function.outer", "@class.outer" },
-        },
-        goto_next_end = {
-          ["]M"] = { "@function.outer", "@class.outer" },
-        },
-        goto_previous_start = {
-          ["[m"] = { "@function.outer", "@class.outer" },
-        },
-        goto_previous_end = {
-          ["[M"] = { "@function.outer", "@class.outer" },
-        },
-      },
-      -- LSP interop
-      lsp_interop = {
-        enable = true,
-        border = "rounded",
-      },
     },
   },
 }
